0cdeaf823828b2bb33dbdcc3d6a10bff87374ce4
k-lusine <lusinekarapetian@gmail.com>
2021-06-13 10:01:27 +0400

    Temporarily disable external repo

--- pages/readme.tsx
+++ pages/readme.tsx
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css';
-import configs from '../data/forum/config.json';
+// import configs from '../data/forum/config.json';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Content from github</h1>
       <p>The content below is powered from Github repo. Feel free to edit <a href="https://github.com/k-lusine/forum/blob/master/config.json"> this file</a> and refresh the page</p>
-      <pre>{JSON.stringify(configs)}</pre>
+      {/* <pre>{JSON.stringify(configs)}</pre> */}
       </div>
   )
 }
