4e3a1074fc0556b6bccdbf01b885eaee28d6fd34
k-lusine <lusinekarapetian@gmail.com>
2021-06-13 15:23:19 +0400

    Experiment incremental ssg

--- pages/readme.tsx
+++ pages/readme.tsx
@@ -1,12 +1,28 @@
 import styles from '../styles/Home.module.css';
 import configs from '../country-config/config.json';
 
-export default function Home() {
+export default function Readme({date}: {date: string}) {
   return (
     <div className={styles.container}>
       <h1>Content from github</h1>
       <p>The content below is powered from Github repo. Feel free to edit <a href="https://github.com/k-lusine/forum/blob/master/config.json"> this file</a> and refresh the page</p>
       <pre>{JSON.stringify(configs)}</pre>
+
+      <div>This page uses incremental SSG. Last updated on {date}</div>
       </div>
   )
+}
+
+export async function getStaticProps() {
+  const date = Date.now();
+
+  return {
+    props: {
+      date,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  }
 }
